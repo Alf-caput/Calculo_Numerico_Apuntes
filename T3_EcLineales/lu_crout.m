77c5caa3b092c283f9e4eb62e429a4f35c294f44
Alf-caput <alfre.roble.aba@gmail.com>
2023-03-05 21:49:27 +0100

    LU CROUT maybe done

--- T3_EcLineales/lu_crout.m
+++ T3_EcLineales/lu_crout.m
@@ -13,9 +13,20 @@ function [L, U] = lu_crout(A)
     U(1, 2:c) = A(1, 2:c) / L(1, 1);
     
     for i = 2: f % 
-        for j = 2: i % número de L
+        for j = 2: i % nº de iteraciones = nº de L a obtener en la fila
             res = 0;
-            L(i, j) = A(i, j) - L(j, k) * U(k, j);
+            for k = 1: j-1  % número de restas para obtener L
+                res = res + L(j, k) * U(k, j);
+            end
+            L(i, j) = A(i, j) - res;
+        end
+        res = 0;
+%         for j = f-i: -1: 1 % j = número de U a obtener en la fila
+        for j = 2: f-i+1 % nº de iteraciones = nº de U a obtener en la fila
+            for k = 1: i-1 % número de restas en numerador para obtener U
+                res = res + L(i, k)*U(k, j+1);
+            end
+            U(i, j+1) = (A(i, j+1) - res) / L(i, i);
         end
     end
 
