fb578b65c1393a346320ebc0bff6e8d0bbc5e626
Alf-caput <alfre.roble.aba@gmail.com>
2023-03-05 20:21:07 +0100

    L of crout

---
+++ T3_EcLineales/cholesky.m
@@ -0,0 +1,4 @@
+function G = cholesky(A)
+% blablabla
+
+end
--- T3_EcLineales/lu_crout.m
+++ T3_EcLineales/lu_crout.m
@@ -11,8 +11,19 @@ function [L, U] = lu_crout(A)
     L(:, 1) = A(:, 1);
     U = eye(f, c);
     U(1, 2:c) = A(1, 2:c) / L(1, 1);
-    for j = 2: i
+    
+    for i = 2: f % 
+        for j = 2: i % número de L
+            res = 0;
+            for k = 1: j-1 % número de restas para obtener L
+                res = res - L(j, k) * U(k, j);
+            end
+            L(i, j) = A(i, j) - L(j, k) * U(k, j);
+        end
     end
+
+
+
 %     for j = 1: f-1
 %         for i = j + 1: f
 %             mul = A(i, j) / A(j, j); % multiplicadores método Gauss
