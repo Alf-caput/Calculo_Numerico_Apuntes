b463a7925fa2b73d95a705c5d840234934cc58d8
Alf-caput <alfre.roble.aba@gmail.com>
2023-03-05 23:52:56 +0100

    fixed sustituir hacia delante

--- T3_EcLineales/sust_adelante.m
+++ T3_EcLineales/sust_adelante.m
@@ -1,10 +1,10 @@
-function x = sust_adelante(Ub)
-    [f, c] = size(Ub);
+function x = sust_adelante(Lb)
+    [f, c] = size(Lb);
     x = zeros(f, 1);
     % Despejamos la primera componente de la solución
-    x(1) = Ub(1, 1) / Ub(1, c);
+    x(1) = Lb(1, c) / Lb(1, 1);
     for i = 2: f
         % Se realiza sustitución hacia delante
-        x(i)= (Ub(i, c) - Ub(i, 1:i-1)*x(1:i-1)) / Ub(i, i);
+        x(i)= (Lb(i, c) - Lb(i, 1:i-1)*x(1:i-1)) / Lb(i, i);
     end
 end
--- T3_EcLineales/sust_atras.m
+++ T3_EcLineales/sust_atras.m
@@ -1,10 +1,10 @@
-function x = sust_atras(Lb)
-    [f, c] = size(Lb);
+function x = sust_atras(Ub)
+    [f, c] = size(Ub);
     x = zeros(f, 1);
     % Despejamos la última componente de la solución
-    x(f) = Lb(f, c) / Lb(f, f);
+    x(f) = Ub(f, c) / Ub(f, f);
     for i = f-1:-1:1
         % Se realiza sustitución hacia atrás
-        x(i)= (Lb(i, c) - Lb(i, i+1:f)*x(i+1:f)) / Lb(i, i);
+        x(i)= (Ub(i, c) - Ub(i, i+1:f)*x(i+1:f)) / Ub(i, i);
     end
 end
