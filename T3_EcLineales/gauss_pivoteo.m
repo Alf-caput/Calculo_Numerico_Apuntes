cf938309e75946e7fa6574fc656efd5d5edf5ef2
Alf-caput <alfre.roble.aba@gmail.com>
2023-03-05 17:08:24 +0100

    gauss pivoteo

--- T3_EcLineales/gauss_pivoteo.m
+++ T3_EcLineales/gauss_pivoteo.m
@@ -1,24 +1,26 @@
-function x = gauss_simple(A, b)
+function x = gauss_pivoteo(A, b)
 % Función que permite calcular la solución aproximada de un sistema lineal.
-% El algoritmo utilizado es el Método de Gauss simple.
+% El algoritmo utilizado es el Método de Gauss con pivoteo.
 % Inputs: 
 % A = matriz de coeficientes
 % b = vector columna de términos independientes
 % Outputs:
 % x = solución aproximada
-% Se dan problemas si: 
-% - Algún pivote es 0
-% - El pivote tiene un valor pequeño en relación al resto de la fila
-% - El sistema está mal condicionado
+% Se asume que el sistema Ax=b introducido es compatible determinado
     % Se define la matriz ampliada
     Ab = [A, b];
     [f, c] = size(Ab);
     for j = 1: f-1
+        % Hallamos el pivote que tiene valor absoluto mayor
+        [~, pos] = max(abs(Ab(:, j)));
+        % Intercambiamos filas (fila j por fila pos)
+        Ab([j, pos], :) = Ab([pos, j], :);
         for i = j + 1: f
             % Se hace ceros debajo del pivote, para ello a las filas de la
             % j+1 a la f se les resta la fila pivote dividida por el pivote
             % y multiplicada por el primer coeficiente no nulo de estas
             Ab(i, j:c) = Ab(i, j:c) -  Ab(i, j) / Ab(j, j) * Ab(j, j:c);
+            disp(Ab)
         end
     end
     x = zeros(f, 1);
